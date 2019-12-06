5967dc962b7b83fed310a3172c93b9bde8ccf8b8
Colin But <colin.but@outlook.com>
2019-12-06 14:35:51 +0000

    main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+    backend "s3" {
+        bucket = "cb-microservice-aws-ec2-docker-setup"
+        key     = "ec2-infra"
+        region  = "eu-west-2"
+    }
+    required_version = ">= 0.12.4"
+}
+
+provider "aws" {
+    region = "eu-west-2"
+}
