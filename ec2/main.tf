92a9e6a3edbd63b82eb7e4f9d1cdb08d80d435ae
Colin But <colin.but@outlook.com>
2020-01-12 19:46:17 +0000

    start docker after provisioning

--- ec2/main.tf
+++ ec2/main.tf
@@ -8,6 +8,10 @@ data "aws_ami" "packer_built_docker_ami" {
     }
 }
 
+data "template_file" "instance_userdata" {
+    template = "${file("${path.cwd}/ec2/instance-bootstrap.tpl")}"
+}
+
 resource "aws_instance" "ec2_instance" {
     ami                         = "${data.aws_ami.packer_built_docker_ami.id}"
     count                       = 2
@@ -15,6 +19,7 @@ resource "aws_instance" "ec2_instance" {
     key_name                    = "${var.key_name}"
     associate_public_ip_address = true
     vpc_security_group_ids      = ["sg-0f89322a79aa4a404"] # ensure security group has SSH access
+    user_data                   = "${data.template_file.instance_userdata.template}"
     
     tags = {
         Name          = "Microservice-Server-Docker"
